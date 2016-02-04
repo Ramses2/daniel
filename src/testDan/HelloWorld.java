2feea5cd48ec5f354c608ae6e866ef0be38208af
DanMatula <matula.dan.catalin@gmail.com>
2016-02-04 10:40:35 +0200

    cmit test 2

--- src/testDan/HelloWorld.java
+++ src/testDan/HelloWorld.java
@@ -5,8 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
-		System.out.println("my name is Dan");
-		System.out.println("this line is added for test");
+		
 		
 		
 
